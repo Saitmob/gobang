5a5568c79df0e942bf8d01b44027f97f94c601e1
qxp <qinxuepeng@lawbbd.com>
2019-09-25 16:49:23 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,197 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="canvas-box" style="width: 520px;">
+      <canvas ref="canvas" height="600" width="600" style="border: 1px #eae8e8 solid"></canvas>
+      <canvas ref="canvasBG" height="600" width="600" class="canvas-bg"></canvas>
+    </div>
   </div>
 </template>
 
 <script>
+const HEIGHT = 50
+const WIDTH = 50
+const start_x = 50
+const start_y = 50
+const box_w = 500
+const box_h = 500
+const dash_box_wh = 40
+
+var ctx = null
+var lastPoint = null
+var nowPoint = null
+
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      piecePoints: [],
+      whitePiecePoints: [],
+      blackPiecePoints: [],
+      currentPlayer: 'black'
+    }
+  },
+  mounted(){
+    this.$nextTick(()=>{
+      let canvas = this.$refs.canvas
+      ctx = canvas.getContext ? canvas.getContext('2d') : {}
+      canvas.addEventListener('click', (e) => {
+        let { x, y } = lastPoint
+
+        if (this.piecePoints.find(v => v.x === x && v.y === y )) {
+          return
+        }
+
+        nowPoint = lastPoint
+        this.piecePoints.push(nowPoint)
+        this[`${this.currentPlayer}PiecePoints`].push(nowPoint)
+
+        console.log('piecePoints', this.piecePoints)
+        this.clearSelectBox(x, y, dash_box_wh)
+        this.paintPiece(x, y, ctx)
+      })
+      canvas.addEventListener('mousemove', (event) => {
+        let { layerX, layerY } = event
+        this.paintBox(layerX, layerY, ctx)
+        if(event.region) {
+          alert('hit region: ' + event.region);
+        }
+      });
+      this.paintGrid(box_w, box_h)
+      ctx = canvas.getContext ? canvas.getContext('2d') : {}
+        // this.paintDashSelect(60, 60, ctx, 40)
+      //   var rectangle = new Path2D();
+      //   rectangle.rect(start_x, start_y, box_w, box_h);
+      //   ctx.stroke(rectangle);
+    })
+  },
+  methods: {
+    // 画背景
+    paintGrid(boxWidth, boxHeight) {
+      let canvas = this.$refs.canvasBG
+      let ctx = canvas.getContext('2d')
+      // 画竖线
+      for(let i = 0; i <= boxWidth / WIDTH; i++) {
+        let x = i * WIDTH + start_x
+        let y0 = start_y
+        let y1 = boxHeight + start_y
+        // debugger;
+        ctx.moveTo(x, y0)
+        ctx.lineTo(x, y1)
+      }
+      // 画横线
+      for(let i = 0; i <= boxHeight / HEIGHT; i++) {
+        let y = i * HEIGHT + start_y
+        let x0 = start_x
+        let x1 = boxHeight + start_x
+        // debugger;
+        ctx.moveTo(x0, y)
+        ctx.lineTo(x1, y)
+      }
+      ctx.stroke();
+    },
+    paintBox(layerX, layerY, ctx){
+      let wh = dash_box_wh
+      // 计算中心点 使中心点吸附在格子的交叉点上 格子的半径为 25
+      // y 轴临界点为 start_y + 25, start_y + 25 + 50, 即 25的倍数，吸附点为0，50，100，所以坐标不到25按0算，25-50却不到50按50算
+      let x_bs = Math.round((layerX - start_x) / (WIDTH))
+      let y_bs = Math.round((layerY - start_y) / (HEIGHT))
+      let x = x_bs * WIDTH + start_x
+      let y = y_bs * WIDTH + start_y
+      // console.log('layerX', layerX, 'layerY', layerY)
+      // console.log('x_bs', x_bs, 'y_bs', y_bs)
+      x = x < 0 ? start_x : x
+      y = y < 0 ? start_y : y
+      // console.log('now',x, y)
+
+      
+      if (lastPoint) {
+        let { x: lx, y: ly } = lastPoint
+        // console.log(lx - wh / 2, ly - wh / 2)
+        // if (x === lx && y === ly) {
+        //   return
+        // }
+        // 如果已点击，则表示棋子已下，则不清空
+        // 鼠标经过曾经下过的棋子，也不清空
+        let lastPointHas = !!this.piecePoints.find(v => v.x === lx && v.y === ly)
+        console.log('lastPointHas', lastPointHas)
+        // debugger;
+        if (!lastPointHas) {
+          this.clearSelectBox(lx, ly, wh)
+        }
+        console.log('last', lx, ly)
+        // console.log('clear', lx - wh / 2, ly - wh / 2)
+      }
+      
+      lastPoint = {
+        x,
+        y
+      }
+      // 如果该点已经有棋子，则不作处理
+      if(this.piecePoints.find(v => v.x === x && v.y === y)) {
+        return
+      }
+      // 超出棋盘不绘制
+      if (layerX < start_x || layerX > box_w + start_x || layerY < start_y || layerY > box_h + start_y) {
+        return
+      }
+      
+
+      this.paintDashSelect(x - wh / 2, y - wh / 2, ctx, wh)
+      
+    },
+    // 鼠标所在点，x, y 为左上角坐标
+    paintDashSelect(x, y, ctx, width) {
+      let lineW = width / 3
+      // 左上角折线
+      // debugger;
+      ctx.beginPath()
+      ctx.moveTo(x, y + lineW)
+      ctx.lineTo(x, y)
+      ctx.lineTo(x + lineW, y)
+      // 右上角折线
+      ctx.moveTo(x + width, y + lineW)
+      ctx.lineTo(x + width, y)
+      ctx.lineTo(x + width - lineW, y)
+      // 左下角
+      ctx.moveTo(x, y + width - lineW)
+      ctx.lineTo(x, y + width)
+      ctx.lineTo(x + lineW, y + width)
+      // 右下角
+      ctx.moveTo(x + width, y + width - lineW)
+      ctx.lineTo(x + width, y + width)
+      ctx.lineTo(x + width - lineW, y + width)
+      // TODO: 将此绘制缓存成图片，提高绘制性能
+      ctx.stroke();
+    },
+    // 清空选择虚线框
+    clearSelectBox(x, y, wh) {
+      ctx.clearRect(x - wh / 2 - 2, y - wh / 2 - 2, wh + 3, wh + 3);
+    },
+    // 描绘棋子
+    paintPiece(x, y, ctx) {
+      ctx.beginPath()
+      ctx.arc(x, y, 20, 0, 2 * Math.PI)
+      
+      if (this.currentPlayer === 'black') {
+        ctx.fillStyle = 'black'
+        this.currentPlayer = 'white'
+      } else {
+        ctx.fillStyle = 'white'
+        this.currentPlayer = 'black'
+      }
+      ctx.fill()
+      ctx.stroke()
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 h3 {
   margin: 40px 0 0;
 }
@@ -54,5 +205,18 @@ li {
 }
 a {
   color: #42b983;
+}
+.hello{
+  display: flex;
+  justify-content: center;
+  .canvas-box{
+    position: relative;
+    .canvas-bg{
+      position: absolute;
+      top: 0;
+      left: 0;
+      z-index: -1;
+    }
+  }
 }
 </style>
